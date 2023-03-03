4c7881cbbf0bc16f0c48d9a279091142867278cd
Muhammad Anas <anassohail34343@gmail.com>
2023-03-03 11:35:14 -0800

    object 5
    Assignments has been complleted till object 5

--- object5.py
+++ object5.py
@@ -1,6 +1,7 @@
 # Write a program that takes input an integer number and print all of its Divisors.
 
 import math
+
 num = int(input("Enter a number to find all divisors"))
 x = math.ceil(num/2) + 1
 for i in range(1, x):
