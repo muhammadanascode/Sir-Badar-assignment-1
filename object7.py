e8344c7258f058c0a7210e74b928fdebd0bf70ba
Muhammad Anas <anassohail34343@gmail.com>
2023-03-03 12:02:26 -0800

    Object7
    Object 7 of Sir badar assignment has been completed

---
+++ object7.py
@@ -0,0 +1,16 @@
+# Extend the logic of last object to write a program that takes input two integer numbers and print highest common divisor
+
+num1 = int((input("Enter a 1st number: \n")))
+num2 = int(input("Enter a sceond number: \n"))
+hcf = 0
+
+if (num1 == 0 or num2 == 0):
+    print("Enter a number greater than 0")
+
+else:
+    x = num1 if num1 < num2 else num2
+    for i in range(1, x+1):
+        if (num1 % i == 0 and num2 % i == 0):
+            hcf = i
+
+    print(f"The hcf of a {num1} and {num2} is {i}")
