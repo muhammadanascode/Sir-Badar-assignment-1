5f3eb98f0850708d26d6202ea48b11c14054dfcd
Muhammad Anas <anassohail34343@gmail.com>
2023-03-04 12:03:22 -0800

    Object2
    Object 2 updated

--- object2.py
+++ object2.py
@@ -6,12 +6,6 @@ factorial = 1
 if(num<0):
     print("Enter a postive integer")
 
-elif (num == 0 and num == 1):
-    print(f"The factorial of a number is {factorial}")
-
-elif (num == 2):
-    print(f"The factorial of a number is", 2)
-
 else:
     for i in range(num, 1, -1):
         factorial *= i
