f439baf9793fbcf6caf5640fa99ebdcd8e746a5d
Muhammad Anas <anassohail34343@gmail.com>
2023-03-03 21:36:03 -0800

    Object 10
    Object 10 has been completed

---
+++ object10.py
@@ -0,0 +1,17 @@
+# Take two inputs from user and check the an number of series lies between given inputs 
+
+
+x = int(input("Enter the first number of a range:\n"))
+y = int(input("Enter the second number of a range:\n"))
+c = False
+for i in range(1,10 , 1):
+    a = i*i-1
+    for j in range(x,y+1):
+        if(a == j):
+            print(f"The range contains {j}")
+            c = True
+
+if(c == False):
+    print(f"No number lies between {x} and {y}")
+
+
