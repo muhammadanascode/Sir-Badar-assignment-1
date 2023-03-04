b410f6e0629ae25179ade2e59aba9363396d7cfa
Muhammad Anas <anassohail34343@gmail.com>
2023-03-03 21:55:54 -0800

    object 4
    object 4 updated

--- object4.py
+++ object4.py
@@ -2,12 +2,8 @@
 # of a number is an even number. The numbers obtained should be printed in a comma-separated
 # sequence
 
-li = []
 
 for num in range(100, 401):
-    c = ""
-    a = [int(i) for i in str(num)]
-    if ((a[0] % 2 == 0 or a[0] % 2 == 2) and (a[1] % 2 == 0 or a[1] % 2 == 2) and (a[2] % 2 == 0 or a[2] % 2 == 2)):
-        for j in a:
-            c += str(j)
-        print(int(c))
+    a = str(num)
+    if (int(a[0]) % 2 == 0 and int(a[1]) % 2 == 0 and int(a[2]) % 2 == 0 ):
+        print(int(a))
