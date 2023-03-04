41757dabe3af33e9c722df523d8c9b7fd26fc574
Muhammad Anas <anassohail34343@gmail.com>
2023-03-04 14:41:23 -0800

    Object3
    Object3 Updated

--- object3.py
+++ object3.py
@@ -3,4 +3,4 @@
 datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class":'V', "section":'A'}]
 
 for i in datalist:
-   print(type(i))
+   print(f"The type of {i} is {type(i)}")
