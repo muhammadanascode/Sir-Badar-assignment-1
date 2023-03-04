859fc75fb4b68d15d8b4c406aea85b4a0f13475e
Muhammad Anas <anassohail34343@gmail.com>
2023-03-03 21:58:17 -0800

    Object4
    updated

--- object4.py
+++ object4.py
@@ -2,7 +2,7 @@
 # of a number is an even number. The numbers obtained should be printed in a comma-separated
 # sequence
 
-
+print("The series is given which contain all even number between 100 and 400 are:\n")
 for num in range(100, 401):
     a = str(num)
     if (int(a[0]) % 2 == 0 and int(a[1]) % 2 == 0 and int(a[2]) % 2 == 0 ):
