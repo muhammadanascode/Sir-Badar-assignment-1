e77f5e1b6edc4b3fa7bb864782e31ee8217f5ff3
Muhammad Anas <anassohail34343@gmail.com>
2023-03-03 17:58:02 -0800

    updated object9
    Object 9 updated

--- object9.py
+++ object9.py
@@ -1,5 +1,3 @@
 #print series wit logic 0,3,8,15,24,35,48,63,80,99
-count = 0
-for i in range(1,112 , 2):
-    print(count)
-    count = (i + 2) + count
+for i in range(1,112 , 1):
+    print(i*i-1)
