37b701c9be6da6f92eda846d2afc3bda1e662b18
Muhammad Anas <anassohail34343@gmail.com>
2023-03-03 21:40:24 -0800

    object10
    Object 10 updated

--- object10.py
+++ object10.py
@@ -4,13 +4,14 @@
 x = int(input("Enter the first number of a range:\n"))
 y = int(input("Enter the second number of a range:\n"))
 c = False
+
 for i in range(1,10):
     a = i*i-1
     for j in range(x,y+1):
         if(a == j):
             print(f"The range contains {j}")
             c = True
-
+            
 if(c == False):
     print(f"No number lies between {x} and {y}")
 
